3e4e82c6f72b65ed91290005952b4e5f10f9c3b3
Maximilian Harrisson <maximilianharrisson@gmail.com>
2024-01-04 22:14:06 -0300

    :art: Move constants to a new class

--- lib/app/core/constants.dart
+++ lib/app/core/constants.dart
@@ -1,11 +1,13 @@
-// API
-const apiVersion = "3";
-const baseUrl = "https://api.themoviedb.org/$apiVersion";
+abstract class Constants {
+  // API
+  static const apiVersion = "3";
+  static const baseUrl = "https://api.themoviedb.org/$apiVersion";
 
-// HTTP
-const maxRetries = 3;
-const retryDelay = 1000; // 1 second
-const httpTimeout = 10000; // 10 seconds
-const httpUnauthorizedErrorMessage = "Unauthorized access error.";
-const httpServerErrorMessage = "Server error.";
-const httpNoConnectionErrorMessage = "No internet connection.";
+  // HTTP
+  static const maxRetries = 3;
+  static const retryDelay = 1000; // 1 second
+  static const httpTimeout = 10000; // 10 seconds
+  static const httpUnauthorizedErrorMessage = "Unauthorized access error.";
+  static const httpServerErrorMessage = "Server error.";
+  static const httpNoConnectionErrorMessage = "No internet connection.";
+}
